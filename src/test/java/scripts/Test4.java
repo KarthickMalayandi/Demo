37b5c4325f2f0d50f40ff7503dcc397ed1e48b81
91877 <91877@LAPTOP-BNEF5TES>
2022-12-10 16:39:50 +0530

    Child B01

---
+++ src/test/java/scripts/Test4.java
@@ -0,0 +1,29 @@
+package scripts;
+
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.SkipException;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.Status;
+
+import genericLibExtentReports.Baseclass;
+
+public class Test4 extends Baseclass {
+	
+	
+	@Test
+	public void tc4()
+	{
+		
+		Reporter.log("TC4",true);
+		
+		
+	}
+	
+	
+
+}
